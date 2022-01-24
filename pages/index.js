164a4f3d3329e6d96ef0c8f5615518c86733a433
MajorTom <ivansantander2020@gmail.com>
2022-01-23 20:59:53 -0500

    my-first-hello-humans-web3

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,55 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useCallback,useEffect } from 'react'
 import { useWeb3React } from '@web3-react/core'
+import { connector } from '../config/web3'
 
 export default function Home() {
   const { 
     activate,
-    acitve,
+    active,
     deactivate,
-    account,
     error,
+    account,
     chainId
   } =  useWeb3React()
 
-  userEffect = (() => {
-    
-  })
+  const connect = useCallback(() => {
+    activate(connector)
+    localStorage.setItem('previouslyConnected',true)
+  },[activate])
 
-  const connect = () => {
+
+  useEffect (() => {
+    if(localStorage.getItem('previouslyConnected')===true)
+    connect()
+  }, [connect])
+
   
+  const disconnect = () => {
+    deactivate
+    localStorage.removeItem('previouslyConnected')
   }
 
-  const disconnect = () => {
-  
+  if(error){
+    return <p>error, {error}</p>
   }
   return (
     <div className={styles.container}>
-        <h1>web3 demo</h1>
-        <button onClick={connect}>Connect wallet</button>
+        <h1>Hi humans, web3 demo.</h1>
+
+        {
+          active 
+            ? <>
+              <button onClick={disconnect}>Disconnect wallet</button>
+              <p>
+                You are connect to network ID: {chainId} <br/>
+                You are account is {account}
+              </p>
+              </>
+
+            : <button onClick={connect}>Connect wallet</button>
+        }
+        
     </div>
   )
 }
